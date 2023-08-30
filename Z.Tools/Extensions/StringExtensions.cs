35d1711137b6d3e67e27cd49a05279d819be94d8
LL zhang <zhang31666@outlook.com>
2023-08-30 09:38:54 +0800

    新增对字符串进行分割的功能

---
+++ Z.Tools/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Z.Tools.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 对字符串以','进行分割，分割后只有一个字符串则返回原字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static dynamic GetPath(this string value)
+        {
+            string[] values = value.Split(',');
+            if (values.Length < 2)
+            {
+                return value;
+            }
+            else
+            {
+                return values;
+            }
+        }
+    }
+}
