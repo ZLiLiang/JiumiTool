aa2563d06ca32d4f94bc182318c6b403b664c6a5
LL zhang <zhang31666@outlook.com>
2024-04-08 23:33:39 +0800

    新增配置服务

---
+++ Z.JuimiTool/Common/IConfigureService.cs
@@ -0,0 +1,10 @@
+﻿namespace Z.JuimiTool.Common
+{
+    public interface IConfigureService
+    {
+        /// <summary>
+        /// 配置初始化
+        /// </summary>
+        void Configure();
+    }
+}
