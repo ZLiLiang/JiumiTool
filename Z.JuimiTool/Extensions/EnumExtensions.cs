44597a41482763106fe328961fc327e7444e2fbd
LL zhang <zhang31666@outlook.com>
2024-04-08 23:34:06 +0800

    枚举扩展

---
+++ Z.JuimiTool/Extensions/EnumExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Z.JuimiTool.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static List<string> ToList<T>() where T : Enum
+        {
+            var enums = Enum.GetValues(typeof(T))
+                .Cast<Enum>();
+            var result = new List<string>();
+            foreach (var item in enums)
+            {
+                result.Add(item.GetDescription());
+            }
+
+            return result;
+        }
+
+        public static string GetDescription(this Enum value)
+        {
+            var fieldInfo = value.GetType()
+                .GetField(value.ToString());
+            var attribute = fieldInfo?.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+            return attribute?.Description ?? value.ToString();
+        }
+    }
+}
