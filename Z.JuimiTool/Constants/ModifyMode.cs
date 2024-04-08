68c346825516765e523494270775cccd5cfcc2ac
LL zhang <zhang31666@outlook.com>
2024-04-08 23:33:59 +0800

    新增修改模式枚举

---
+++ Z.JuimiTool/Constants/ModifyMode.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Z.JuimiTool.Constants
+{
+    public enum ModifyMode
+    {
+        [Description("文件模式")]
+        File,
+
+        [Description("文件夹模式")]
+        Folder
+    }
+}
