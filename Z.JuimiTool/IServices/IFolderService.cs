e6b3e9b0bc3bced70404edb012c1222b215f7529
LL zhang <zhang31666@outlook.com>
2024-04-09 12:28:29 +0800

    新增继承资源接口的文件和文件夹接口

---
+++ Z.JuimiTool/IServices/IFolderService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Z.JuimiTool.Models;
+
+namespace Z.JuimiTool.IServices
+{
+    public interface IFolderService : IResourceService
+    {
+        /// <summary>
+        /// 获取路径下的文件夹
+        /// </summary>
+        /// <param name="path">单个路径</param>
+        /// <returns></returns>
+        public List<Folder> GetFolders(string path);
+
+        /// <summary>
+        /// 获取多个路径的文件夹
+        /// </summary>
+        /// <param name="paths">多个路径</param>
+        /// <returns></returns>
+        public List<Folder> GetFolders(IEnumerable<string> paths);
+    }
+}
