f1a4a9a79c0d3750b53a9c0a48a9363ca1bba278
LL zhang <zhang31666@outlook.com>
2024-04-09 10:30:55 +0800

    新增字符串扩展

---
+++ Z.JuimiTool/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Z.JuimiTool.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 对路径以','进行分割
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static List<string> GetPaths(this string value)
+        {
+            return value.Split(',').ToList();
+        }
+    }
+}
