f9716f9b81d776b3c198aa6b132ac8ab38aecd76
LL zhang <zhang31666@outlook.com>
2024-04-09 10:57:48 +0800

    新增去除括号的字符串扩展

--- Z.JuimiTool/Extensions/StringExtensions.cs
+++ Z.JuimiTool/Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Z.JuimiTool.Extensions
 {
@@ -13,6 +14,24 @@ namespace Z.JuimiTool.Extensions
         public static List<string> GetPaths(this string value)
         {
             return value.Split(',').ToList();
+        }
+
+        /// <summary>
+        /// 删除文件名的括号以及括号的内容
+        /// </summary>
+        /// <param name="srcName">原文件名称</param>
+        /// <param name="count">编号</param>
+        /// <returns></returns>
+        public static string RemoveParentheses(string srcName, int count)
+        {
+            string fileName = Regex.Replace(srcName, @"\([^)]*\)", ""); // 去除括号及内容
+
+            fileName = Regex.Replace(fileName, @"(?:\s|\d)+(?=\.|\s|$)", math =>
+            {
+                return count.ToString();
+            }); //对文件名进行编号
+
+            return fileName;
         }
     }
 }
