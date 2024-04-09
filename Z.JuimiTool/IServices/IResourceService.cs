7dcda554ca2d4cbf8e20c064e8a1f4d234e217a5
LL zhang <zhang31666@outlook.com>
2024-04-09 12:28:13 +0800

    修改资源接口

--- Z.JuimiTool/IServices/IResourceService.cs
+++ Z.JuimiTool/IServices/IResourceService.cs
@@ -6,20 +6,6 @@ namespace Z.JuimiTool.IServices
     public interface IResourceService
     {
         /// <summary>
-        /// 获取路径下的资源
-        /// </summary>
-        /// <param name="path">单个路径</param>
-        /// <returns></returns>
-        public List<Resource> GetResources(string path);
-
-        /// <summary>
-        /// 获取多个路径下的资源
-        /// </summary>
-        /// <param name="paths">多个路径</param>
-        /// <returns></returns>
-        public List<Resource> GetResources(IEnumerable<string> paths);
-
-        /// <summary>
         /// 重命名资源
         /// </summary>
         /// <param name="resources">资源列表</param>
