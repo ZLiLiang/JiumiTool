545816f91120c428a1a1b3cf3ebb6725cd70f595
LL zhang <zhang31666@outlook.com>
2024-04-09 10:57:56 +0800

    新增服务接口

---
+++ Z.JuimiTool/IServices/IResourceServices.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Z.JuimiTool.Models;
+
+namespace Z.JuimiTool.IServices
+{
+    public interface IResourceServices
+    {
+        /// <summary>
+        /// 获取路径下的资源
+        /// </summary>
+        /// <param name="path">单个路径</param>
+        /// <returns></returns>
+        public List<Resource> GetResources(string path);
+
+        /// <summary>
+        /// 获取多个路径下的资源
+        /// </summary>
+        /// <param name="paths">多个路径</param>
+        /// <returns></returns>
+        public List<Resource> GetResources(IEnumerable<string> paths);
+
+        /// <summary>
+        /// 重命名资源
+        /// </summary>
+        /// <param name="resources">资源列表</param>
+        /// <returns>路径列表</returns>
+        public List<string> RepeatName(IEnumerable<Resource> resources);
+
+        /// <summary>
+        /// 备份资源
+        /// </summary>
+        /// <param name="resources">资源列表</param>
+        /// <returns>路径</returns>
+        public string BackUp(IEnumerable<Resource> resources);
+    }
+}
