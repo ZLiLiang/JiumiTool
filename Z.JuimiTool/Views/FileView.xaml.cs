5aee28faca5dba0046c37180e1d1f45d1cba7cda
LL zhang <zhang31666@outlook.com>
2024-04-10 13:43:21 +0800

    新增修改按钮不可以使用

--- Z.JuimiTool/Views/FileView.xaml.cs
+++ Z.JuimiTool/Views/FileView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Z.JuimiTool.Constants;
+using Z.JuimiTool.ViewModels;
 
 namespace Z.JuimiTool.Views
 {
