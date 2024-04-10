c094095d362afb8ed9a43f010ed4ccff3ac2e62c
LL zhang <zhang31666@outlook.com>
2024-04-10 13:43:46 +0800

    新增文件路径框的拖拽导入

--- Z.JuimiTool/ViewModels/FileViewModel.cs
+++ Z.JuimiTool/ViewModels/FileViewModel.cs
@@ -4,14 +4,37 @@ using Prism.Commands;
 using Prism.Mvvm;
 using Z.JuimiTool.Extensions;
 using Z.JuimiTool.Constants;
+using Microsoft.Win32;
+using Z.JuimiTool.Models;
+using Z.JuimiTool.IServices;
+using System.Linq;
+using System.Windows;
+using Prism.Services.Dialogs;
+using System;
 
 namespace Z.JuimiTool.ViewModels
 {
     public class FileViewModel : BindableBase
     {
+        private IFileService fileService;
+        private IFolderService folderService;
         private int selectedMode = 0;
         private string filePath = string.Empty;
+        private bool isBackup = false;
+        private bool isModify = false;
         private ObservableCollection<string> message = new ObservableCollection<string>();
+        /// <summary>
+        /// 待修改队列
+        /// </summary>
+        private Queue<Resource> modifyResources { get; set; } = new Queue<Resource>();
+        /// <summary>
+        /// 唯一资源字典
+        /// </summary>
+        private Dictionary<string, Resource> uniqueResources = new Dictionary<string, Resource>();
+        /// <summary>
+        /// 唯一路径
+        /// </summary>
+        private HashSet<string> uniquePath = new HashSet<string>();
 
         /// <summary>
         /// 文件/文件夹模式
@@ -26,6 +49,15 @@ namespace Z.JuimiTool.ViewModels
         /// </summary>
         public string FilePath { get => filePath; set => SetProperty(ref filePath, value); }
         /// <summary>
+        /// 是否备份
+        /// </summary>
+        public bool IsBackup { get => isBackup; set => SetProperty(ref isBackup, value); }
+        /// <summary>
+        /// 是否修改
+        /// </summary>
+        public bool IsModify { get => isModify; set => SetProperty(ref isModify, value); }
+
+        /// <summary>
         /// 信息列表
         /// </summary>
         public ObservableCollection<string> Message
@@ -37,12 +69,18 @@ namespace Z.JuimiTool.ViewModels
         public DelegateCommand SelectCommand { get; private set; }
         public DelegateCommand ModifyCommand { get; private set; }
         public DelegateCommand ClearCommand { get; private set; }
+        public DelegateCommand<DragEventArgs> DragDropCommand { get; private set; }
+        public DelegateCommand<DragEventArgs> PreviewDragOverCommand { get; private set; }
 
-        public FileViewModel()
+        public FileViewModel(IFileService fileService, IFolderService folderService)
         {
             SelectCommand = new DelegateCommand(SelectExecute);
             ModifyCommand = new DelegateCommand(ModifyExecute);
             ClearCommand = new DelegateCommand(ClearExecute);
+            DragDropCommand = new DelegateCommand<DragEventArgs>(DragDropExecute);
+            PreviewDragOverCommand = new DelegateCommand<DragEventArgs>(PreviewDragOverExecute);
+            this.fileService = fileService;
+            this.folderService = folderService;
         }
 
         #region 私有执行
@@ -52,7 +90,35 @@ namespace Z.JuimiTool.ViewModels
         /// </summary>
         private void SelectExecute()
         {
-            
+            ClearExecute();
+            OpenFolderDialog dialog = new OpenFolderDialog();
+            dialog.Title = "选择要处理的文件夹";
+
+            if (dialog.ShowDialog() == true)
+            {
+                FilePath = dialog.FolderName;
+                var result = new List<Resource>();
+                if (SelectedMode == (int)ModifyMode.File)
+                {
+                    //获取目录下的所有文件
+                    result = fileService.GetFiles(dialog.FolderName)
+                        .Cast<Resource>()
+                        .ToList();
+                }
+                else
+                {
+                    //获取目录下的所有文件夹
+                    result = folderService.GetFolders(dialog.FolderName)
+                        .Cast<Resource>()
+                        .ToList();
+                }
+                foreach (var item in result)
+                {
+                    Message.Add(item.Name);
+                    modifyResources.Enqueue(item);
+                }
+                IsModify = true;
+            }
         }
 
         /// <summary>
@@ -64,12 +130,73 @@ namespace Z.JuimiTool.ViewModels
         }
 
         /// <summary>
-        /// 清除listbox与textblock的内容
+        /// 清除listbox、textblock和队列的内容
         /// </summary>
         private void ClearExecute()
         {
             FilePath = string.Empty;
             Message.Clear();
+            modifyResources.Clear();
+            uniqueResources.Clear();
+            uniquePath.Clear();
+            IsModify= false;
+        }
+
+        /// <summary>
+        /// textbox拖拽文件
+        /// </summary>
+        /// <param name="e"></param>
+        private void DragDropExecute(DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var item in files)
+                {
+                    uniquePath.Add(item);
+                }
+                files = uniquePath.ToArray();
+                FilePath = string.Join(",", files);
+
+
+                var result = new List<Resource>();
+                if (SelectedMode == (int)ModifyMode.File)
+                {
+                    //获取目录下的所有文件
+                    result = fileService.GetFiles(files)
+                        .Cast<Resource>()
+                        .ToList();
+                }
+                else
+                {
+                    result = folderService.GetFolders(files)
+                            .Cast<Resource>()
+                            .ToList();
+                }
+
+                foreach (var item in result)
+                {
+                    uniqueResources.TryAdd(item.FullName, item);
+                }
+                Message.Clear();
+                modifyResources.Clear();
+                foreach (var item in uniqueResources.Values)
+                {
+                    Message.Add(item.Name);
+                    modifyResources.Enqueue(item);
+                }
+
+                IsModify = true;
+            }
+        }
+
+        /// <summary>
+        /// 允许进行拖拽
+        /// </summary>
+        /// <param name="e"></param>
+        private void PreviewDragOverExecute(DragEventArgs e)
+        {
+            e.Handled = true;
         }
 
         #endregion
