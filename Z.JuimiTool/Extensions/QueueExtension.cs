2a1853fae2e988d652f428ab87e459321340dabb
LL zhang <zhang31666@outlook.com>
2024-04-10 14:40:21 +0800

    队列出队列转换为列表扩展

---
+++ Z.JuimiTool/Extensions/QueueExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Z.JuimiTool.Extensions
+{
+    public static class QueueExtension
+    {
+        /// <summary>
+        /// 将队列的元素全部移除
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        public static List<T> DequeueToList<T>(this Queue<T> queue)
+        {
+            var result = new List<T>(queue.Count);
+            while (queue.TryDequeue(out T element))
+            {
+                result.Add(element);
+            }
+
+            return result;
+        }
+    }
+}
