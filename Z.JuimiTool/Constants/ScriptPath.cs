e53b72535a2975c03ebd9fd64f54dc6eda746942
LL zhang <zhang31666@outlook.com>
2024-04-14 18:49:07 +0800

    注入脚本和常量路径

---
+++ Z.JuimiTool/Constants/ScriptPath.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Z.JuimiTool.Constants
+{
+    public static class ScriptPath
+    {
+        public static readonly string InjectionScript = $"{Environment.CurrentDirectory}/Script/InjectionScript.js";
+    }
+}
