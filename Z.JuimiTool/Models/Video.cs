d88bba06423895e9398df833d1c774d853d3ee92
LL zhang <zhang31666@outlook.com>
2024-04-14 18:49:31 +0800

    视频模型

---
+++ Z.JuimiTool/Models/Video.cs
@@ -0,0 +1,40 @@
+﻿namespace Z.JuimiTool.Models
+{
+    public class Video
+    {
+        /// <summary>
+        /// 上传者
+        /// </summary>
+        public string Uploader { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 大小
+        /// </summary>
+        public string Size { get; set; }
+
+        /// <summary>
+        /// 封面链接
+        /// </summary>
+        public string ThumbUrl { get; set; }
+
+        /// <summary>
+        /// 下载链接
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 密钥
+        /// </summary>
+        public ulong Decodekey { get; set; }
+
+        /// <summary>
+        /// 解密数组
+        /// </summary>
+        public byte[] DecryptionArray { get; set; }
+    }
+}
