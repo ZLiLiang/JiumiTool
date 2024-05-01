627bf556d3ab798d4fb78e614bcf4b9f243beee3
LL zhang <zhang31666@outlook.com>
2024-05-01 17:59:40 +0800

    解决listbox不能跟随界面缩放问题

--- JiumiTool2/ViewModels/FileViewModel.cs
+++ JiumiTool2/ViewModels/FileViewModel.cs
@@ -1,8 +1,57 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Wpf.Ui.Controls;
 
 namespace JiumiTool2.ViewModels
 {
-    public class FileViewModel : ObservableObject
+    public partial class FileViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<string> _messageItems = [];
+
+        public FileViewModel()
+        {
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+            MessageItems.Add("test");
+        }
+
+        [RelayCommand]
+        private void PageSizeChanged(object sender)
+        {
+            // 确保sender是Page类型，尽管在本方法中sender通常就是当前Page实例
+            if (sender is not Page currentPage)
+            {
+                return;
+            }
+
+            // 使用FindName方法查找子元素
+            ListBox messageListBox = currentPage.FindName("MessageListBox") as ListBox;
+            Border messageBorder = currentPage.FindName("MessageBorder") as Border;
+
+            messageListBox.Height = 0;
+            messageBorder.UpdateLayout();
+            var actualHeight = messageBorder.ActualHeight;
+            messageListBox.Height = actualHeight - 2;
+        }
     }
 }
--- JiumiTool2/Views/FileView.xaml.cs
+++ JiumiTool2/Views/FileView.xaml.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using JiumiTool2.ViewModels;
+using Wpf.Ui.Controls;
 
 namespace JiumiTool2.Views
 {
     /// <summary>
     /// FileView.xaml 的交互逻辑
     /// </summary>
-    public partial class FileView : Page
+    public partial class FileView : INavigableView<FileViewModel>
     {
-        public FileView()
+        public FileViewModel ViewModel { get; }
+
+        public FileView(FileViewModel viewModel)
         {
+            ViewModel = viewModel;
+            DataContext = this;
+
             InitializeComponent();
         }
     }
