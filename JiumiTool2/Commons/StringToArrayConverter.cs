8d05156370be843b6974329fc3964df8f193bb27
LL zhang <zhang31666@outlook.com>
2024-05-03 00:30:08 +0800

    字符串转字符数组转换器

---
+++ JiumiTool2/Commons/StringToArrayConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace JiumiTool2.Commons
+{
+    public class StringToArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                return new ObservableCollection<char>(text);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
