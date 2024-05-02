b154524aaa531d39747320cd3b2658ab45d9d982
LL zhang <zhang31666@outlook.com>
2024-05-03 00:30:21 +0800

    修改模式

---
+++ JiumiTool2/Constants/ModifyMode.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace JiumiTool2.Constants
+{
+    public enum ModifyMode
+    {
+        [Description("文件模式")]
+        File,
+
+        [Description("文件夹模式")]
+        Folder
+    }
+}
