6ef2f001fe647937b0290793098610a3e437fca5
LL zhang <zhang31666@outlook.com>
2024-05-04 10:15:17 +0800

    新增根据描述转枚举

--- JiumiTool2/Extensions/StringExtension.cs
+++ JiumiTool2/Extensions/StringExtension.cs
@@ -1,4 +1,7 @@
-﻿namespace JiumiTool2.Extensions
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace JiumiTool2.Extensions
 {
     public static class StringExtension
     {
@@ -13,6 +16,26 @@
             var result = (TEnum)Enum.Parse(typeof(TEnum), value, true);
 
             return result;
+        }
+
+        public static TEnum DescriptionToEnum<TEnum>(this string value)
+        {
+            var fieldInfos = typeof(TEnum).GetFields();
+            foreach (var item in fieldInfos)
+            {
+                if (!item.FieldType.Equals(typeof(TEnum)))
+                {
+                    continue;
+                }
+                var attribute = item.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+                var description = attribute.Description;
+                if (value.Equals(description))
+                {
+                    return (TEnum)item.GetValue(null);
+                }
+            }
+
+            return default;
         }
     }
 }
