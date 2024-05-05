427a8641ec80f963f0a5f7cb5dcee17baf2e871c
LL zhang <zhang31666@outlook.com>
2024-05-05 17:32:26 +0800

    匹配regex服务

---
+++ JiumiTool2/IServices/IMatchRegexService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using JiumiTool2.Constants;
+
+namespace JiumiTool2.IServices
+{
+    public interface IMatchRegexService
+    {
+        /// <summary>
+        /// 生成正则表达式目标字符串
+        /// </summary>
+        /// <param name="chars"></param>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public string GeneratePattern(List<char> chars, string? pattern = null);
+
+        /// <summary>
+        /// 生成正则表达式的对象
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="modifySeat"></param>
+        /// <returns></returns>
+        public Regex GenerateRegex(string pattern, FileModifySeat modifySeat);
+    }
+}
---
+++ JiumiTool2/Services/MatchRegexService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using JiumiTool2.Constants;
+using JiumiTool2.IServices;
+
+namespace JiumiTool2.Services
+{
+    public class MatchRegexService : IMatchRegexService
+    {
+        public string GeneratePattern(List<char> chars, string? pattern = null)
+        {
+            pattern ??= ".*?";
+            var startChar = chars.First();
+            var endChar = chars.Last();
+            var result = @$"\{startChar}{pattern}\{endChar}";
+
+            return result;
+        }
+
+        public Regex GenerateRegex(string pattern, FileModifySeat modifySeat)
+        {
+            if (modifySeat == FileModifySeat.Prefix)
+            {
+                return new Regex(pattern, RegexOptions.None);
+            }
+            else
+            {
+                return new Regex(pattern, RegexOptions.RightToLeft);
+            }
+        }
+    }
+}
