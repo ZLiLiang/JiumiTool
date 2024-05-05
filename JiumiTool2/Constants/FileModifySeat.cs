c9ec55bd0d4942fa3cfee24929b832ec4b59da70
LL zhang <zhang31666@outlook.com>
2024-05-05 17:38:03 +0800

    匹配位置枚举

---
+++ JiumiTool2/Constants/FileModifySeat.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace JiumiTool2.Constants
+{
+    public enum FileModifySeat
+    {
+        [Description("前缀")]
+        Prefix,
+
+        [Description("后缀")]
+        Suffix
+    }
+}
