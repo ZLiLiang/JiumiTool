1eb88ea0a9375ae5470927c1262628a8e6ef1fab
LL zhang <zhang31666@outlook.com>
2024-05-25 20:31:01 +0800

    资源抽象服务和文件服务接口

---
+++ JiumiTool2/IServices/AResourceService.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using JiumiTool2.Models;
+
+namespace JiumiTool2.IServices
+{
+    public abstract class AResourceService
+    {
+        /// <summary>
+        /// 重命名资源 <br/>
+        /// action的第一个参数是srcFullName，第二个参数是destFullName
+        /// </summary>
+        /// <param name="resources">资源列表</param>
+        /// <param name="regex">正则匹配器</param>
+        /// <param name="action">重命名函数</param>
+        /// <returns>路径列表</returns>
+        public List<string> RepeatName(IEnumerable<Resource> resources, Regex regex, Action<string, string> action)
+        {
+            int startCount = 1;
+            List<string> result = new List<string>();
+
+            foreach (Resource item in resources)
+            {
+                // 跳过包含备份字符串的路径
+                if (item.Name.Contains("备份"))
+                {
+                    continue;
+                }
+
+                var srcFullName = item.FullName;
+                // 对资源名进行重命名并编号
+                var destName = regex.Replace(item.Name, evaluator =>
+                {
+                    return startCount.ToString();
+                });
+                var destFullName = Path.Combine(item.Path, destName);
+
+                // 重命名资源
+                action.Invoke(srcFullName, destFullName);
+                result.Add(string.Join(" -> ", item.Name, destName));
+
+                startCount++;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 备份资源 <br/>
+        /// action的第一个参数是srcFullName，第二个参数是destFullName
+        /// </summary>
+        /// <param name="resources">资源列表</param>
+        /// <param name="action">备份函数</param>
+        /// <returns>路径</returns>
+        public string Backup(IEnumerable<Resource> resources, Action<string, string> action)
+        {
+            var backUpPath = $"{resources.First().Path}\\备份-{DateTime.Now:yyyy/MM/dd HH:mm:ss}";
+            // 创建目录
+            Directory.CreateDirectory(backUpPath);
+
+            foreach (var item in resources)
+            {
+                var srcFullName = item.FullName;
+                var destFullName = Path.Combine(backUpPath, item.Name);
+                // 备份操作
+                action.Invoke(srcFullName, destFullName);
+            }
+
+            return $"备份路径:{backUpPath}";
+        }
+    }
+}
---
+++ JiumiTool2/IServices/IFileService.cs
@@ -0,0 +1,22 @@
+﻿using JiumiTool2.Models;
+
+namespace JiumiTool2.IServices
+{
+    public interface IFileService
+    {
+        /// <summary>
+        /// 获取路径下的文件
+        /// </summary>
+        /// <param name="path">单个路径</param>
+        /// <returns></returns>
+        public List<File> GetFiles(string path);
+
+        /// <summary>
+        /// 获取多个路径的文件 <br/>
+        /// 路径包括文件夹和文件
+        /// </summary>
+        /// <param name="paths">多个路径</param>
+        /// <returns></returns>
+        public List<File> GetFiles(IEnumerable<string> paths);
+    }
+}
