aeda9be16969372918a446fc2f2de1f6b1488d0a
LL zhang <zhang31666@outlook.com>
2024-05-29 17:37:01 +0800

    listbox保持最底部

---
+++ JiumiTool2/ControlBehavior/ListBoxBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.Specialized;
+
+namespace JiumiTool2.ControlBehavior
+{
+    public static class ListBoxBehavior
+    {
+        /// <summary>
+        /// 属性的访问构造器
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool GetAutoScrollToEnd(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollToEndProperty);
+        }
+
+        /// <summary>
+        /// 属性的设置构造器
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetAutoScrollToEnd(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollToEndProperty, value);
+        }
+
+        /// <summary>
+        /// 设置一个属性
+        /// </summary>
+        public static readonly DependencyProperty AutoScrollToEndProperty =
+            DependencyProperty.RegisterAttached(
+                "AutoScrollToEnd",
+                typeof(bool),
+                typeof(ListBoxBehavior),
+                new PropertyMetadata(false, OnAutoScrollToEndChanged));
+
+        private static void OnAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ListBox listBox && (bool)e.NewValue)
+            {
+                listBox.Loaded += (s, ea) =>
+                {
+                    var collectionView = CollectionViewSource.GetDefaultView(listBox.ItemsSource);
+                    if (collectionView is INotifyCollectionChanged notifyCollection)
+                    {
+                        notifyCollection.CollectionChanged += (sender, args) =>
+                        {
+                            listBox.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                if (listBox.Items.Count > 0)
+                                {
+                                    listBox.ScrollIntoView(listBox.Items[listBox.Items.Count - 1]);
+                                }
+                            }));
+                        };
+                    }
+                };
+            }
+        }
+    }
+}
