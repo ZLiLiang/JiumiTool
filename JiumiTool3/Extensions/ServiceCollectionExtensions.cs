9816f1c631b9858b5ebc1cb803dbfb89e9b17f4f
LiLiang <2339431739@qq.com>
2024-10-20 01:19:35 +0800

    注册服务

---
+++ JiumiTool3/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using JiumiTool3.ViewModels;
+using JiumiTool3.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JiumiTool3.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCommonServices(this IServiceCollection collection)
+    {
+        collection.AddTransient<MainViewModel>();
+    }
+}
