d9d6262bffd75aa388b78d3fa6824f4578a6fe33
LiLiang <2339431739@qq.com>
2024-10-20 18:11:26 +0800

    修改容器扩展

--- JiumiTool3/Extensions/ServiceCollectionExtensions.cs
+++ JiumiTool3/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using JiumiTool3.ViewModels;
+﻿using FluentAvalonia.UI.Controls;
+using JiumiTool3.IServices;
+using JiumiTool3.Pages;
+using JiumiTool3.Services;
+using JiumiTool3.ViewModels;
 using JiumiTool3.Views;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,8 +10,19 @@ namespace JiumiTool3.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddCommonServices(this IServiceCollection collection)
+    public static void AddViewAndModelServices(this IServiceCollection collection)
     {
         collection.AddTransient<MainViewModel>();
+        collection.AddTransient<SettingsPageViewModel>();
+
+        collection.AddTransient<MainWindow>();
+        collection.AddTransient<MainView>();
+        collection.AddTransient<SettingsPage>();
+    }
+
+    public static void AddCommonServices(this IServiceCollection collection)
+    {
+        collection.AddSingleton<INavigationPageFactory, NavigationFactory>();
+        collection.AddSingleton<INavigationService, NavigationService>();
     }
 }
